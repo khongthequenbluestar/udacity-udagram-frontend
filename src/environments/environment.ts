3e02e02006d797d608d445f84071aa91910f08d0
Anh Đức <khongthequenbluestar@gmail.com>
2022-08-12 12:43:12 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: false,
   appName: 'Udagram',
   //apiHost: 'http://localhost:8200/api/v0'
-  apiHost: 'http://af633107929d44de688a6bc8198bbedf-1383357965.us-east-2.elb.amazonaws.com:8200/api/v0'
+  apiHost: 'http://a0f1c932dfa2b46818b37b4aeb9189b3-96121857.us-east-2.elb.amazonaws.com:8200/api/v0'
 };
 
 /*
